6e8795a7575f53275f947c4864908d004eb7de01
codealfa <codealfa@users.noreply.gitub.com>
2022-07-02 18:51:52 -0400

    Adding package Exceptions

--- src/Base.php
+++ src/Base.php
@@ -15,106 +15,105 @@ use CodeAlfa\RegexTokenizer\Debug\Debug;
 
 trait Base
 {
-        use Debug;
+	use Debug;
 
-        //language=RegExp
-        public static function DOUBLE_QUOTE_STRING()
-        {
-                return '"' . self::DOUBLE_QUOTE_STRING_VALUE() . '(?:"|(?=$))';
-        }
-
-        //language=RegExp
-        public static function DOUBLE_QUOTE_STRING_VALUE()
-        {
-                return '(?<=")(?>(?:\\\\.)?[^\\\\"]*+)++';
-        }
-
-        //language=RegExp
-        public static function SINGLE_QUOTE_STRING()
-        {
-                return "'" . self::SINGLE_QUOTE_STRING_VALUE() . "(?:'|(?=$))";
-        }
-
-        //language=RegExp
-        public static function SINGLE_QUOTE_STRING_VALUE()
-        {
-                return "(?<=')(?>(?:\\\\.)?[^\\\\']*+)++";
-        }
-
-        //language=RegExp
-        public static function BACK_TICK_STRING()
-        {
-                return '`' . self::BACK_TICK_STRING_VALUE() . '(?:`|(?=$))';
-        }
-
-        //language=RegExp
-        public static function BACK_TICK_STRING_VALUE()
-        {
-                return '(?<=`)(?>(?:\\\\.)?[^\\\\`]*+)++';
-        }
-
-        //language=RegExp
-        public static function STRING_CP( $bCV = false )
-        {
-                $sString = '[\'"`]<<' . self::STRING_VALUE() . '>>[\'"`]';
-
-                return self::prepare( $sString, $bCV );
-        }
-
-        //language=RegExp
-        public static function STRING_VALUE(): string
+	//language=RegExp
+	public static function DOUBLE_QUOTE_STRING()
 	{
-                return '(?:' . self::DOUBLE_QUOTE_STRING_VALUE() . '|' . self::SINGLE_QUOTE_STRING_VALUE() . '|' . self::BACK_TICK_STRING_VALUE() . ')';
-        }
+		return '"' . self::DOUBLE_QUOTE_STRING_VALUE() . '(?:"|(?=$))';
+	}
 
-        //language=RegExp
-        private static function prepare( $sRegex, $bCV )
-        {
-                $aSearchArray = array( '<<<', '>>>', '<<', '>>' );
+	//language=RegExp
+	public static function DOUBLE_QUOTE_STRING_VALUE()
+	{
+		return '(?<=")(?>(?:\\\\.)?[^\\\\"]*+)++';
+	}
 
-                if ( $bCV )
-                {
-                        return str_replace( $aSearchArray, array( '(?|', ')', '(', ')' ), $sRegex );
-                }
-                else
-                {
-                        return str_replace( $aSearchArray, array( '(?:', ')', '', '' ), $sRegex );
-                }
-        }
+	//language=RegExp
+	public static function SINGLE_QUOTE_STRING()
+	{
+		return "'" . self::SINGLE_QUOTE_STRING_VALUE() . "(?:'|(?=$))";
+	}
 
-        //language=RegExp
-        public static function COMMENT()
-        {
-                return '(?:' . self::BLOCK_COMMENT() . '|' . self::LINE_COMMENT() . ')';
-        }
+	//language=RegExp
+	public static function SINGLE_QUOTE_STRING_VALUE()
+	{
+		return "(?<=')(?>(?:\\\\.)?[^\\\\']*+)++";
+	}
 
-        //language=RegExp
-        public static function BLOCK_COMMENT()
-        {
-                return '/\*(?>\*?[^*]*+)*?\*/';
-        }
+	//language=RegExp
+	public static function BACK_TICK_STRING()
+	{
+		return '`' . self::BACK_TICK_STRING_VALUE() . '(?:`|(?=$))';
+	}
 
-        public static function LINE_COMMENT()
-        {
-                return '//[^\r\n]*+';
-        }
+	//language=RegExp
+	public static function BACK_TICK_STRING_VALUE()
+	{
+		return '(?<=`)(?>(?:\\\\.)?[^\\\\`]*+)++';
+	}
 
-        protected static function throwExceptionOnPregError( $sExceptionClassName = '' )
-        {
-                if ( $sExceptionClassName === '' )
-                {
-                        $sExceptionClassName = 'Exception';
-                }
+	//language=RegExp
+	public static function STRING_CP( $bCV = false )
+	{
+		$sString = '[\'"`]<<' . self::STRING_VALUE() . '>>[\'"`]';
 
-                $error = array_flip( array_filter( get_defined_constants( true )[ 'pcre' ], function ( $value )
-                {
-                        return substr( $value, -6 ) === '_ERROR';
-                }, ARRAY_FILTER_USE_KEY ) )[ preg_last_error() ];
+		return self::prepare( $sString, $bCV );
+	}
 
-                if ( preg_last_error() != PREG_NO_ERROR )
-                {
-                        throw new $sExceptionClassName( $error );
-                }
-        }
+	//language=RegExp
+	public static function STRING_VALUE(): string
+	{
+		return '(?:' . self::DOUBLE_QUOTE_STRING_VALUE() . '|' . self::SINGLE_QUOTE_STRING_VALUE() . '|' . self::BACK_TICK_STRING_VALUE() . ')';
+	}
+
+	//language=RegExp
+	private static function prepare( $sRegex, $bCV )
+	{
+		$aSearchArray = array( '<<<', '>>>', '<<', '>>' );
+
+		if ( $bCV )
+		{
+			return str_replace( $aSearchArray, array( '(?|', ')', '(', ')' ), $sRegex );
+		}
+		else
+		{
+			return str_replace( $aSearchArray, array( '(?:', ')', '', '' ), $sRegex );
+		}
+	}
+
+	//language=RegExp
+	public static function COMMENT()
+	{
+		return '(?:' . self::BLOCK_COMMENT() . '|' . self::LINE_COMMENT() . ')';
+	}
+
+	//language=RegExp
+	public static function BLOCK_COMMENT()
+	{
+		return '/\*(?>\*?[^*]*+)*?\*/';
+	}
+
+	public static function LINE_COMMENT()
+	{
+		return '//[^\r\n]*+';
+	}
+
+	protected static function throwExceptionOnPregError( $exceptionClassName = '' )
+	{
+		if ( $exceptionClassName === '' )
+		{
+			$exceptionClassName = 'Exception';
+		}
+
+		$error = array_flip( array_filter( get_defined_constants( true )['pcre'], function ( $value ) {
+			return substr( $value, -6 ) === '_ERROR';
+		}, ARRAY_FILTER_USE_KEY ) )[ preg_last_error() ];
+
+		if ( preg_last_error() != PREG_NO_ERROR )
+		{
+			throw new $exceptionClassName( $error );
+		}
+	}
 
 }
